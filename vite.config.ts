4c44a7daad080e769dcdf39ac5046aeb7fa82365
dainz <mbenjaminarancibia@gmail.com>
2026-09-24 22:52:41 -0300

    deploy on gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   server: { proxy: { "/api/catalog": "http://localhost:8080", "/api/v1/orders": "http://localhost:8081" } },
+  base: "/frontend",
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
