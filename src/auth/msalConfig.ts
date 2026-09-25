d593c9db15300facf3b35e1010dd0b74774e737e
dainz <mbenjaminarancibia@gmail.com>
2026-09-24 23:00:55 -0300

    change base url

--- src/auth/msalConfig.ts
+++ src/auth/msalConfig.ts
@@ -18,7 +18,7 @@ export const msalConfig: Configuration = {
         authority: "https://login.microsoftonline.com/f9bce5c0-eb96-4341-aad7-411ae980b12a",
 
         // Debe coincidir EXACTAMENTE con el Redirect URI de Entra ID
-        redirectUri: "https://cloudnative2026.github.io/frontend/"
+        redirectUri: "https://cloudnative2026.github.io/"
     },
 
     cache: {
